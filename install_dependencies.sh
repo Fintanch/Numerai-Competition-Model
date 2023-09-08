d69acb68eb44f41f12b1042f62912c9b366c2791
Fiorella Ratti <fiorellaratti@Fiorellas-MacBook-Pro.local>
2023-09-08 18:09:09 -0400

    Add data augmentation

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-pip install numerapi
+pip install numerapi matplotlib
 conda install numpy pandas -y
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y
 conda install pyarrow fastparquet -y
