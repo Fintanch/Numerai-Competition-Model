f62347074bcb037ff9836163d3e4d7e42dc1e5a3
Johnnykoch02 <johnnykoch02@gmail.com>
2023-09-14 14:58:47 -0400

    training the Numerai Model

--- Networks/NumeraiPredictionModels.py
+++ Networks/NumeraiPredictionModels.py
@@ -10,7 +10,7 @@ x -> let opinions = {ResidualFeatureEncoder_i(x) for ResidualFeatureEncoder in s
 """
 
 class ResidualBlock(nn.Module):
-    def __init__(self, residual_dim=512, brodcast_dim=1048, dropout_prob=0.1, activation_fnc=nn.GELU, device='cuda'):
+    def __init__(self, residual_dim=512, brodcast_dim=1048, dropout_prob=0.05, activation_fnc=nn.Tanh, device='cuda'):
         super(ResidualBlock, self).__init__()
         self.device = device
         self.to(self.device)
@@ -32,13 +32,13 @@ class ResidualBlock(nn.Module):
         return self._norm(x + self.residual_block(x))
     
 class ResidualFeatureEncoder(nn.Module):
-    def __init__(self, expert_num, num_residuals=5, input_dim=313, output_dim= 256, residual_dim=512, dataset_name="Dataset_563", device='cuda'):
+    def __init__(self, expert_num, num_residuals=5, input_dim=313, output_dim= 256, residual_dim=512, dataset_name="Dataset_568", device='cuda'):
         super(ResidualFeatureEncoder, self).__init__()
         self.device = device
         self.to(self.device)
         self.brodcast = nn.Sequential(
             nn.Linear(input_dim, residual_dim),
-            nn.GELU(),
+            nn.Tanh(),
         )
         self.residuals = [ResidualBlock(residual_dim=residual_dim,) for _ in range(num_residuals)]
         self.residuals = nn.Sequential(*self.residuals)
@@ -78,17 +78,20 @@ class ExpertDecoder(nn.Module):
         self._norm = nn.LayerNorm(residual_output_dim)
         self.decoder = nn.Sequential(
             nn.Linear(residual_output_dim, residual_output_dim),
-            nn.GELU(),
+            nn.Tanh(),
             nn.Dropout(dropout_probs),
             nn.Linear(residual_output_dim, residual_output_dim),
-            nn.GELU(),
+            nn.Tanh(),
+            nn.Dropout(dropout_probs),
+            nn.Linear(residual_output_dim, residual_output_dim),
+            nn.Tanh(),
             nn.Dropout(dropout_probs),
             nn.Linear(residual_output_dim, residual_output_dim//4),
-            nn.GELU(),
+            nn.Tanh(),
             nn.Linear(residual_output_dim//4, residual_output_dim//8),
-            nn.GELU(),
+            nn.Tanh(),
             nn.Linear(residual_output_dim//8, output_dim), # output 
-            nn.Sigmoid(),
+            nn.Sigmoid(), # Not Cross-Entropy Loss
         )
         self._ds_name = dataset_name
         self.save_path = os.path.join('.', f'Networks','Checkpoints','NumeraiExperts', 'ExpertDecoder')
@@ -108,17 +111,23 @@ class ExpertDecoder(nn.Module):
         normalized_consensus = self._norm(expert_consensus)
         y = self.decoder(normalized_consensus)
         return y
+    
+    def predict(self, x):
+        self.eval()
+        output = self(x)
+        cls_pred = th.argmax(output, dim=1)
+        return cls_pred.data.cpu() / 4
 
     def load_checkpoint(self):
-        if os.path.exists(os.path.join(self.save_path, self.file_name)):
-            self.load_state_dict(th.load(os.path.join(self.save_path, self.file_name)))
+        if os.path.exists(os.path.join(self.save_path, self._file_name)):
+            self.load_state_dict(th.load(os.path.join(self.save_path, self._file_name)))
     
     def save_checkpoint(self, file=None):
-        print(f'[Expert {self._expert_num}] Saving Checkpoint...')
+        print(f'[ExpertDecoder] Saving Checkpoint...')
         if file != None:
             th.save(self.state_dict(), file)
         else:
-            th.save(self.state_dict(), self.save_path + "/" + self.file_name)
+            th.save(self.state_dict(), self.save_path + "/" + self._file_name)
 
 # # Testing Network
 # input_size = 313
